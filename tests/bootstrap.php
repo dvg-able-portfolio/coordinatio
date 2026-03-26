ff897e18586095f2ac24083dc6bee7889aeda73e
dvg-able <dirkvgerigk@gmail.com>
2026-03-26 14:01:15 +0100

    test: automate database setup and reset in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
@@ -13,3 +13,23 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+// temporarily - start (will be removed)
+// Reset test database on each test run
+passthru(sprintf(
+    'APP_ENV=test php "%s/../bin/console" doctrine:database:drop --force --if-exists 2>&1',
+    __DIR__
+));
+passthru(sprintf(
+    'APP_ENV=test php "%s/../bin/console" doctrine:database:create 2>&1',
+    __DIR__
+));
+//passthru(sprintf(
+//    'APP_ENV=test php "%s/../bin/console" doctrine:migrations:migrate --no-interaction 2>&1',
+//    __DIR__
+//));
+passthru(sprintf(
+    'APP_ENV=test php "%s/../bin/console" doctrine:schema:update --force --no-interaction 2>&1',
+    __DIR__
+));
+// temporarily - end 
